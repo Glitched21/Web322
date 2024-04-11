c6d722da495dd58d194c7d7388cf165a1bcd8f18
Ritish Sharma <rsharma360@myseneca.ca>
2024-04-11 05:06:29 -0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views//*.ejs'],
+  content: [`./views/**/*.ejs`, `./views/partials/*.ejs`], // all .ejs files
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-    require('@tailwindcss/typography'),
-  ],  
-};
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['fantasy'],
+  },
+}
